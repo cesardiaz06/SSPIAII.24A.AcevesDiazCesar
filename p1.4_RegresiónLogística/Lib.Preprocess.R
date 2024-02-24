e42b1154fb2b5a238125c7dc2a600095157ad935
César Aceves Díaz <cesar.aceves5895@alumnos.udg.mx>
2024-02-24 12:20:55 -0600

    [P1.4] - Script Librerías.

---
+++ p1.4_RegresiónLogística/Lib.Preprocess.R
@@ -0,0 +1,18 @@
+#Librerías.
+libs <- c("ggplot","caTools","mltools","data.table","cowplot","e1071","caret")
+
+#Librerías.
+if(!require(ggplot2))
+{install.packages(libs, dependences = T)}
+
+if(!require(caret))
+{install.packages(libs, dependences = T)}
+
+#Habilitarlas.
+library(ggplot2)
+library(caTools)
+library(mltools)
+library(data.table)
+library(cowplot)
+library(e1071)
+library(caret)
